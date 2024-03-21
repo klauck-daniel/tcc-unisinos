1100fa700143ad22040016ade06c39838042f462
Daniel Klauck <daniel_klauck@hotmail.com>
2024-03-20 23:35:53 -0300

    first commit

---
+++ ESP32/automated_test_equipment_firmware/main/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+// https://www.youtube.com/watch?v=Yhg9maZjqiw
+//
+
+void app_main(void)
+{
+}
---
+++ Interface/teste.py
@@ -0,0 +1,6 @@
+from guizero import App, Text
+
+app = App(title="Hello World")
+message = Text(app, text="Menssagem")
+
+app.display()
